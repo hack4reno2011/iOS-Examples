50de45356f634a047e7b586f412c44eab229ca7a
John Jusayan <john@treeness.com>
2011-10-13 17:00:00 -0700

    Initial commit

---
+++ Geolocation/Geolocation/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Geolocation
+//
+//  Created by John Jusayan on 10/13/11.
+//  Copyright (c) 2011 Treeness, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Geolocation/Geolocation/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Geolocation
+//
+//  Created by John Jusayan on 10/13/11.
+//  Copyright (c) 2011 Treeness, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
